dea55664c7d5bc84af84c5a9a8b2f9a72d379592
Md. Rezaul Karim <rezaulrahaat@gmail.com>
2026-07-06 12:54:25 +0600

    fix(userLogin): improve code formatting and readability

--- client/src/services/actions/userLogin.ts
+++ client/src/services/actions/userLogin.ts
@@ -1,36 +1,31 @@
 // "use server";
 
-import { FieldValues } from 'react-hook-form';
-import setAccessToken from './setAccessToken';
+import { FieldValues } from "react-hook-form";
+import setAccessToken from "./setAccessToken";
 
 export const userLogin = async (data: FieldValues) => {
-   const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/login`,
-      {
-         method: 'POST',
-         headers: {
-            'Content-Type': 'application/json',
-         },
-         body: JSON.stringify(data),
-         credentials: 'include',
-         // cache: "no-store",
-      }
-   );
-   const userInfo = await res.json();
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+      credentials: "include",
+      // cache: "no-store",
+    },
+  );
+  const userInfo = await res.json();
 
+  if (userInfo?.data?.accessToken) {
+    const passwordChangeRequired = userInfo.data.needsPasswordChange;
 
+    setAccessToken(userInfo.data.accessToken, {
+      redirect: "/dashboard",
+      passwordChangeRequired,
+    });
+  }
 
-   const passwordChangeRequired = userInfo.data.needsPasswordChange;
-
-
-
-   if (userInfo.data.accessToken) {
-      setAccessToken(userInfo.data.accessToken, {
-         redirect: '/dashboard',
-         passwordChangeRequired,
-      });
-   }
-
-
-   return userInfo;
+  return userInfo;
 };
